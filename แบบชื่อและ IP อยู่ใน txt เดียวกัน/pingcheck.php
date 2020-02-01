10861b8958587c0db2c52dcbf0910e8365f52a79
superogira <superogira@hotmail.com>
2020-02-02 01:22:26 +0700

    Update pingcheck.php

--- แบบชื่อและ IP อยู่ใน txt เดียวกัน/pingcheck.php
+++ แบบชื่อและ IP อยู่ใน txt เดียวกัน/pingcheck.php
@@ -36,7 +36,7 @@ function DateThai($strDate)
 {
 	$strYear = date("Y",strtotime($strDate))+543;
 	$strMonth= date("n",strtotime($strDate));
-		$strDay= date("j",strtotime($strDate));
+	$strDay= date("j",strtotime($strDate));
 	$strHour= date("H",strtotime($strDate));
 	$strMinute= date("i",strtotime($strDate));
 	$strSeconds= date("s",strtotime($strDate));
